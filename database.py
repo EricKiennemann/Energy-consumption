7066b55bdd1e95f927e0b343932f3fa1c2e67b31
eric <eric@padam.io>
2021-04-06 15:09:25 +0200

    constants

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 import psycopg2
+from collections import defaultdict
+from datetime import datetime
+from constants import DB_OLD_DATE
 
 class Bat_iris(object):
 
@@ -17,7 +20,7 @@ class Bat_iris(object):
             user=self.user,
             password=self.password)
 
-    def get_bat_iris(self):
+    def _get_bat_iris(self):
         # create a cursor
         cur = self.conn.cursor()
 
@@ -25,15 +28,13 @@ class Bat_iris(object):
         cur.execute(
             """
             select iris.code_iris,
-		    bat.usage1,
-		    bat.usage2,
-		    date_creat,
-		    nb_logts,
+		    bat.date_creat,
+		    bat.nb_logts,
 		    bat.geom2d2
             from public."BATIMENT" as bat
             JOIN public."IRIS_GE" AS iris
             ON ST_Contains(iris.geom, bat.geom)
-            where (usage1 = 'Résidentiel' or usage2 = 'Résidentiel')
+            where (usage1 = 'Résidentiel' or usage2 = 'Résidentiel') and nb_logts > 0
             """
         )
 
@@ -43,6 +44,22 @@ class Bat_iris(object):
 
         return rows
 
+    def _dispatch_by_bat(self):
+        old_date = datetime.strptime(DB_OLD_DATE,'%d/%m/%y')
+        bat_dispatch = defaultdict(dict())
+        rows = self._get_bat_iris()
+        for row in rows:
+            if row[2] == 1:
+                if datetime.strptime(row[1], '%y-%m/%d %H:%M:%S') > old_date:
+                    bat_dispatch[row[0]]['h_new'] +=1
+                else:
+                    bat_dispatch[row[0]]['h_old'] += 1
+            elif datetime.strptime(row[1], '%y-%m/%d %H:%M:%S') > old_date:
+                    bat_dispatch[row[0]]['c_new'] += row[2]
+            else:
+                    bat_dispatch[row[0]]['c_old'] += row[2]
+        return bat_dispatch
+
 if __name__ == '__main__':
     base = Bat_iris("Energy")
-    print(base.get_bat_iris())
+    print(base._dispatch_by_bat())
